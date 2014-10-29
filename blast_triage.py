6258a1219b9844dab08b096e1c4f4377772da44f
Diogo Silva <o.diogosilva@gmail.com>
2014-10-29 12:25:00 +0000

    Initialized argparser

--- blast_triage.py
+++ blast_triage.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python2
 
+import argparse
+from Bio.Blast.Applications import NcbiblastxCommandline
 
+parser = argparse.ArgumentParser(description="Application that removes sequences "
+											"potentially contaminant sequences from a "
+											"file using BLAST on a custom set of genomes")
+
+arg = parser.parse_args()
 
