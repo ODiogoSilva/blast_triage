b822407777b5e382901dce98e86386796b8ae8a3
Diogo Silva <o.diogosilva@gmail.com>
2014-10-29 14:40:03 +0000

    Implemented triage function with a simple triage rule

--- blast_triage.py
+++ blast_triage.py
@@ -43,14 +43,40 @@ def blast_wrapper(input_file, reference_database, contaminant_database):
 			# Executing BLAST for each specified database
 			for db in reference_database:
 				result = blast_worker(temp_query, db)
-				reference_results.append(result)
+				if result is not None:
+					reference_results.append(result)
 
 			for db in contaminant_database:
 				result = blast_worker(temp_query, db)
-				contaminant_results.append(result)
+				if result is not None:
+					contaminant_results.append(result)
 
 	file_handle.close()
 
+
+def triage(reference_result, contaminant_result):
+	"""
+	From two lists containing the BLAST results for the reference and contaminant
+	databases, this function compares the evalue and identity of the query sequence
+	:param reference_result: List containing the blast results of the reference databases
+	:param contaminant_result: List containing the blast results of the contaminant
+	databases
+	:return: Either True, if there is no contamination or False, if there is contamination
+	"""
+
+	#reference_evalues_max = max([float(x[2]) for x in reference_result])
+	reference_ident_max = max([float(x[3]) for x in reference_result])
+
+	#contaminant_evalues_max = max([float(x[2]) for x in contaminant_result])
+	contaminant_ident_max = max([float(x[3]) for x in contaminant_result])
+
+	if reference_ident_max > contaminant_ident_max:
+
+		return True
+
+	else:
+
+		return False
 
 def blast_worker(query_file, blast_database):
 	"""
