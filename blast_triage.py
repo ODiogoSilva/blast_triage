746bd53c54a48850c2806955644318cc3999a541
Diogo Silva <o.diogosilva@gmail.com>
2014-11-05 16:44:10 +0000

    Added support for fasta files

--- blast_triage.py
+++ blast_triage.py
@@ -4,30 +4,34 @@ import argparse
 import subprocess
 from Bio.Blast.Applications import NcbiblastxCommandline
 
-parser = argparse.ArgumentParser(description="Application that removes sequences "
-                    "potentially contaminant sequences from a "
+parser = argparse.ArgumentParser(description="Application that removes "
+                    "sequences potentially contaminant sequences from a "
                     "file using BLAST on a custom set of genomes")
 
-parser.add_argument("-in", dest="infile", required=True, help="Provide the FastQ input "
-                    "file")
-parser.add_argument("-r", dest="reference_database", nargs="*", help="Provide the "
-                    "path to the reference BLAST databases")
-parser.add_argument("-c", dest="contaminant_database", nargs="*", help="Provide the "
-                    "path to the contaminant BLAST databases")
-parser.add_argument("-o", dest="output_file", help="Provide the name of the output file")
+parser.add_argument("-in", dest="infile", required=True, help="Provide the "
+                    "FastQ input file")
+parser.add_argument("-r", dest="reference_database", nargs="*", help="Provide "
+                    "the path to the reference BLAST databases")
+parser.add_argument("-c", dest="contaminant_database", nargs="*", help="Provide"
+                    " the path to the contaminant BLAST databases")
+parser.add_argument("-o", dest="output_file", help="Provide the name of the"
+                    " output file")
 
 arg = parser.parse_args()
 
 
-def blast_wrapper(input_file, reference_database, contaminant_database, output_file):
+def blast_wrapper(input_file, reference_database,
+                  contaminant_database, output_file):
     """
     :param input_file: String with the input file name
-    :param reference_database: List containing the path of the reference databases
-    :param contaminant_database: List containing the path of the contaminant databases
-    Main function of the BLAST phase of the script. It iterates over a sequence file in
-    Fasta format and for each sequence it will BLAST it on the custom blast databases.
-    The results of the blasts for the different databases will be parsed and compared
-    to sort the sequences into contaminants or normal.
+    :param reference_database: List containing the path of the reference
+    databases
+    :param contaminant_database: List containing the path of the contaminant
+    databases
+    Main function of the BLAST phase of the script. It iterates over a sequence
+    file in Fasta format and for each sequence it will BLAST it on the custom
+    blast databases. The results of the blasts for the different databases will
+    be parsed and compared to sort the sequences into contaminants or normal.
     :return:
     """
 
@@ -38,7 +42,7 @@ def blast_wrapper(input_file, reference_database, contaminant_database, output_f
     # Start iterating over input file
     for line in file_handle:
 
-        if line.startswith("@"):
+        if line.startswith("@") or line.startswith(">"):
             # Retrieving sequence and sequence name
             sequence_code = line[1:].strip()
             sequence = file_handle.next().strip()
@@ -66,15 +70,16 @@ def blast_wrapper(input_file, reference_database, contaminant_database, output_f
             # Triage
             result = triage(reference_results, contaminant_results)
 
-            # The try statement accommodates the scenario where there are only blast
-            #  hits on the contaminant database
+            # The try statement accommodates the scenario where there are only
+            #  blast hits on the contaminant database
             try:
                 reference_ident = max([float(x[3]) for x in reference_results])
             except (IndexError, ValueError):
                 reference_ident = "None"
 
             try:
-                contaminant_ident = max([float(x[3]) for x in contaminant_results])
+                contaminant_ident = max([float(x[3]) for x in
+                                        contaminant_results])
             except (IndexError, ValueError):
                 contaminant_ident = "None"
 
@@ -82,12 +87,16 @@ def blast_wrapper(input_file, reference_database, contaminant_database, output_f
                 print("\rProcessing sequence ID %s -> Winner is reference %s" %
                     (sequence_code, reference_ident))
                 output_handle.write(">%s\n%s\n" % (sequence_code, sequence))
-                log_handle.write("%s; %s; %s\n" % (sequence_code, reference_ident, contaminant_ident))
+                log_handle.write("%s; %s; %s\n" % (sequence_code,
+                                                   reference_ident,
+                                                   contaminant_ident))
 
             else:
-                print("\rProcessing sequence ID %s -> Winner is contaminant %s" %
-                    (sequence_code, contaminant_ident))
-                log_handle.write("%s; %s; %s\n" % (sequence_code, reference_ident, contaminant_ident))
+                print("\rProcessing sequence ID %s -> Winner is contaminant %s"
+                      % (sequence_code, contaminant_ident))
+                log_handle.write("%s; %s; %s\n" % (sequence_code,
+                                                   reference_ident,
+                                                   contaminant_ident))
 
             temp_handle.close()
             # Removing temporary file
@@ -100,21 +109,25 @@ def blast_wrapper(input_file, reference_database, contaminant_database, output_f
 
 def triage(reference_result, contaminant_result):
     """
-    From two lists containing the BLAST results for the reference and contaminant
-    databases, this function compares the evalue and identity of the query sequence
-    :param reference_result: List containing the blast results of the reference databases
-    :param contaminant_result: List containing the blast results of the contaminant
+    From two lists containing the BLAST results for the reference and
+    contaminant databases, this function compares the evalue and identity of
+    the query sequence
+    :param reference_result: List containing the blast results of the reference
     databases
-    :return: Either True, if there is no contamination or False, if there is contamination
+    :param contaminant_result: List containing the blast results of the
+    contaminant
+    databases
+    :return: Either True, if there is no contamination or False, if there is
+    contamination
     """
 
-    # In case the query sequence has blast hits on the contaminant but not in the
-    # reference
+    # In case the query sequence has blast hits on the contaminant but not in
+    # the reference
     if reference_result == [] and contaminant_result != []:
         return False
 
-    # In case the query sequence has not hits in neither database, the benefit of doubt
-    #  is given and the sequence is saved
+    # In case the query sequence has not hits in neither database,
+    # the benefit of doubt is given and the sequence is saved
     if reference_result == [] and contaminant_result == []:
         return True
 
@@ -143,12 +156,12 @@ def blast_worker(query_file, blast_database):
     :return:
     """
 
-    # Defining the blast command without specifying the output file name, redirects the
-    # output to stdout, which can be parsed within the script
+    # Defining the blast command without specifying the output file name,
+    # redirects the output to stdout, which can be parsed within the script
     blastx_cline = NcbiblastxCommandline(cmd="blastn", query=query_file,
-                                        db=blast_database, evalue=0.001, outfmt="'6 "
-                                        "qseqid sseqid evalue pident'",
-                                        out="temp.csv", num_descriptions=1)
+                                        db=blast_database, evalue=0.001,
+                                        outfmt="'6 qseqid sseqid evalue pident'"
+                                         , out="temp.csv", num_descriptions=1)
 
     blastx_cline()
 
@@ -177,7 +190,8 @@ def main():
     output_file = arg.output_file
 
     # Execution
-    blast_wrapper(input_file, reference_databases, contaminant_databases, output_file)
+    blast_wrapper(input_file, reference_databases, contaminant_databases,
+                  output_file)
 
 
 main()
